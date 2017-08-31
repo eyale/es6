9cd54f9e5a915cf5a42b7ae8a5ff1dc8d2418886
Honcharov Anton <honcharovanton@MacBook-Pro-Honcharov.local>
2017-09-01 01:28:52 +0300

    preparation

--- wp-output-management/src/index.js
+++ wp-output-management/src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('div'),
+      btn = document.createElement('button')
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
+  btn.innerHTML = 'Click and check console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn)
+
   return element;
 }
   
