03c1dadf091f8fed7086341602f20a93fa6ec103
Honcharov Anton <honcharovanton@MacBook-Pro-Honcharov.local>
2017-09-05 23:35:42 +0300

    adding webpack watch

--- wp-output-management/src/print.js
+++ wp-output-management/src/print.js
@@ -1,3 +1,4 @@
 export default function printMe () {
-    consolee.log('I get called from print.js!')
+    console.log('I get called from print.js!')
+    console.info('webpack watch is running')
 }
