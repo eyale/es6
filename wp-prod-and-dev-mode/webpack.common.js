ff053b900596d999aeec784eab0f93431998149e
Honcharov Anton <honcharovanton@MacBook-Pro-Honcharov.local>
2017-09-13 00:24:47 +0300

    fixing webpack for prod

--- wp-prod-and-dev-mode/webpack.common.js
+++ wp-prod-and-dev-mode/webpack.common.js
@@ -1,25 +1,7 @@
-// const path = require('path')
-// const CleanWebpackPlugin = require('clean-webpack-plugin')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-// module.exports = {
-//     entry: {
-//         app: './src/index.js'
-//     },
-//     plugin: [
-//         new CleanWebpackPlugin(['dist']),
-//         new HtmlWebpackPlugin({ title: 'Production' })
-//     ],
-//     output: {
-//         filename: '[name].bundle.js',
-//         path: path.resolve(__dirname, 'dist')
-//     }
-// }
-
-
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     app: './src/index.js'
